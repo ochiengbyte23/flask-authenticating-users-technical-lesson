b1b0560b2cf49f054be024e435b446d73eeb4b64
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-04-22 12:58:33 +0300

    final solution

--- server/app.py
+++ server/app.py
@@ -16,7 +16,39 @@ db.init_app(app)
 
 api = Api(app)
 
-# Define routes
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+        
+        if user:   
+            session['user_id'] = user.id
+            
+            return UserSchema().dump(user)
+        else:
+            return {'message': 'Invalid Login'}, 401
+            
+api.add_resource(Login, '/login')
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return UserSchema().dump(user)
+        else:
+            return {'message': '401: Not Authorized'}, 401
+        
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
